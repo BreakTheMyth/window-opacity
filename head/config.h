cf7697c2f7770854099997b486116af6b41058e7
BreakTheMyth <pinks302@163.com>
2025-04-06 13:35:52 +0800

    add LICENSE

--- head/config.h
+++ head/config.h
@@ -1,5 +1,5 @@
-//                                                     记事本      QQ                    任务栏           Firefox               资源管理器
-#define /*窗口的标题*/             WINDOW_TITLE         NULL,      NULL,                 NULL,            NULL,                 NULL
-#define /*窗口的类名*/             WINDOW_CLASS         "Notepad", "Chrome_WidgetWin_1", "Shell_TrayWnd", "MozillaWindowClass", "CabinetWClass"
-#define /*窗口失去焦点时的透明度*/  WINDOW_OPACITY       192,       192,                  192,             192,                  192
-#define /*窗口获得焦点时的透明度*/  WINDOW_FOCUS_OPACITY 224,       224,                  192,             224,                  224
+//                                                     记事本      QQ                    任务栏           Firefox               资源管理器        
+#define /*窗口的标题*/             WINDOW_TITLE         NULL,      NULL,                 NULL,            NULL,                 NULL,            
+#define /*窗口的类名*/             WINDOW_CLASS         "Notepad", "Chrome_WidgetWin_1", "Shell_TrayWnd", "MozillaWindowClass", "CabinetWClass", 
+#define /*窗口失去焦点时的透明度*/  WINDOW_OPACITY       192,       192,                  192,             192,                  192,             
+#define /*窗口获得焦点时的透明度*/  WINDOW_FOCUS_OPACITY 224,       224,                  192,             224,                  224,             
